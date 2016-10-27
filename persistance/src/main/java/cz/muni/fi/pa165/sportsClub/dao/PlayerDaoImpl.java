948ee67608e489482e99e119218415401d42465d
Simon Sudora <Simon Sudora>
2016-10-27 03:12:24 +0200

    add player entity

---
+++ persistance/src/main/java/cz/muni/fi/pa165/sportsClub/dao/PlayerDaoImpl.java
@@ -0,0 +1,7 @@
+package cz.muni.fi.pa165.sportsClub.dao;
+
+/**
+ * Created by Simon on 27.10.2016.
+ */
+public class PlayerDaoImpl {
+}
---
+++ persistance/src/main/java/cz/muni/fi/pa165/sportsClub/pojo/PlayerPojo.java
@@ -0,0 +1,7 @@
+package cz.muni.fi.pa165.sportsClub.pojo;
+
+/**
+ * Created by Simon on 27.10.2016.
+ */
+public class PlayerPojo {
+}
