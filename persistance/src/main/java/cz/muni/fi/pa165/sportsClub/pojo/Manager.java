1625c22ec53c926b097d8c817868d418ded9b302
Jan Tomášek <yan.tomasek@gmail.com>
2016-10-30 21:09:40 +0100

    reference to set of teams

--- persistance/src/main/java/cz/muni/fi/pa165/sportsClub/pojo/Manager.java
+++ persistance/src/main/java/cz/muni/fi/pa165/sportsClub/pojo/Manager.java
@@ -1,10 +1,14 @@
 package cz.muni.fi.pa165.sportsClub.pojo;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 import javax.validation.constraints.NotNull;
 
@@ -13,19 +17,29 @@ import javax.validation.constraints.NotNull;
  */
 @Entity
 public class Manager {
+
 	@Id
 	@GeneratedValue(strategy=GenerationType.AUTO)
 	private Long id;
+
 	private String firstName;
+
 	private String lastName;
+
 	@Column(nullable=false,unique=true)
 	private String email;
+
 	private String mobile;
+
 	@Column(nullable = false)
 	private String password;
+
 	@OneToOne(mappedBy="manager")
 	@NotNull
 	private Club club;
+
+	@OneToMany
+	private Set<Team> teams = new HashSet<>();
 
 	public Long getId() {
 		return this.id;
@@ -81,5 +95,22 @@ public class Manager {
 
 	public void setClub(Club club) {
 		this.club = club;
+	}
+
+	public Set<Team> getTeams() {
+		return teams;
+	}
+
+	public void addTeam(Team team) {
+		teams.add(team);
+	}
+
+	public void removeTeam(Team team) {
+		teams.remove(team);
+	}
+
+	public void updateTeam(Team team) {
+		teams.remove(team);
+		teams.add(team);
 	}
 }
