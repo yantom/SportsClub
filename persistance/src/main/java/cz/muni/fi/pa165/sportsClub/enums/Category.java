9c110c072258594ca917b0e251452219a64d896c
Simon Sudora <simon.sudora@gmail.com>
2016-11-20 16:33:11 +0100

    Update Category enum

--- persistance/src/main/java/cz/muni/fi/pa165/sportsClub/enums/Category.java
+++ persistance/src/main/java/cz/muni/fi/pa165/sportsClub/enums/Category.java
@@ -6,6 +6,16 @@ package cz.muni.fi.pa165.sportsClub.enums;
  * @author Andrej 410433
  */
 public enum Category {
-	MEN, U19, U17, U15, U13;
+	MEN(Integer.MAX_VALUE), U19(19), U17(17), U15(15), U13(13);
+
+	private int ageLimit;
+
+	private Category(int ageLimit){
+		this.ageLimit = ageLimit;
+	}
+
+	public int getAgeLimit(){
+		return ageLimit;
+	}
 }
 
