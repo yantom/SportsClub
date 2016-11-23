43eb67495a70737566a3c6fb07f017ff9e097e24
AndrejBony <andrej.bonis@gmail.com>
2016-11-23 12:47:21 +0100

    PlayerServiceImpl (not complete, one method left)

--- service/src/main/java/cz/muni/fi/pa165/service/PlayerServiceImpl.java
+++ service/src/main/java/cz/muni/fi/pa165/service/PlayerServiceImpl.java
@@ -1,5 +1,6 @@
 package cz.muni.fi.pa165.service;
 
+import cz.muni.fi.pa165.sportsClub.dao.ClubDao;
 import java.util.List;
 
 import javax.inject.Inject;
@@ -9,42 +10,51 @@ import cz.muni.fi.pa165.sportsClub.pojo.Club;
 import cz.muni.fi.pa165.sportsClub.pojo.Player;
 import cz.muni.fi.pa165.sportsClub.pojo.PlayerInfo;
 
+/**
+ * Represents service layer of player
+ * 
+ * @author Andrej Bonis 410433
+ */
 public class PlayerServiceImpl implements PlayerService {
-	@Inject
-	PlayerDao playerDao;
 
-	public void createPlayer(Player p) {
-		// TODO Auto-generated method stub
+    @Inject
+    PlayerDao playerDao;
+    
+    @Inject
+    ClubDao clubDao;
 
-	}
+    public void createPlayer(Player p) {
+        playerDao.createPlayer(p);
 
-	public void updatePlayer(Player p) {
-		// TODO Auto-generated method stub
+    }
 
-	}
+    public void updatePlayer(Player p) {
+        playerDao.updatePlayer(p);
 
-	public void deletePlayer(Player p) {
-		// TODO Auto-generated method stub
+    }
 
-	}
+    public void deletePlayer(Player p) {
+        playerDao.deletePlayer(p);
 
-	public Player getPlayerById(Long playerId) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    }
 
-	public Player getPlayerByEmail(String email) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    public Player getPlayerById(Long playerId) {
+        return playerDao.getPlayerById(playerId);
+    }
 
-	public List<Player> getAllPlayersOfClub(Club c) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    public Player getPlayerByEmail(String email) {
+        //TODO
+        return null;
+    }
 
-	public List<PlayerInfo> getPlayerInfos(Player p) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    public List<Player> getAllPlayersOfClub(Club c) {
+        Club club = clubDao.getClubById(c.getId());
+        return club.getManager().getPlayers();
+
+    }
+
+    public List<PlayerInfo> getPlayerInfos(Player p) {
+        Player player = playerDao.getPlayerById(p.getId());
+        return player.getPlayerInfos();
+    }
 }
