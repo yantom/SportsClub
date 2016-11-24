b938e2b87e86cd9515edc98769257b6896bcdf99
Simon Sudora <simon.sudora@gmail.com>
2016-11-24 18:53:58 +0100

    fix last commit

--- service/src/test/java/cz/muni/fi/pa165/sportsClub/test/ClubServiceTest.java
+++ service/src/test/java/cz/muni/fi/pa165/sportsClub/test/ClubServiceTest.java
@@ -2,29 +2,28 @@ package cz.muni.fi.pa165.sportsClub.test;
 
 
 import cz.muni.fi.pa165.sportsClub.dao.ClubDao;
+import cz.muni.fi.pa165.sportsClub.pojo.Player;
 import cz.muni.fi.pa165.sportsClub.service.ClubService;
 import cz.muni.fi.pa165.sportsClub.service.ServiceApplicationContext;
+import org.hibernate.service.spi.ServiceException;
+import org.junit.BeforeClass;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
+import org.mockito.MockitoAnnotations;
 import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.testng.AbstractTransactionalTestNGSpringContextTests;
-
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import javax.inject.Inject;
-
-import static org.mockito.Matchers.any;
-import static org.mockito.Mockito.verify;
 
 /**
  * @author Simon Sudora 461460
  */
+
+@RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = ServiceApplicationContext.class)
-public class ClubServiceTest extends AbstractTransactionalTestNGSpringContextTests {
+public class ClubServiceTest {
 
     @Mock
     ClubDao clubDao;
@@ -33,10 +32,10 @@ public class ClubServiceTest extends AbstractTransactionalTestNGSpringContextTes
     @InjectMocks
     private ClubService clubService;
 
-//    @BeforeClass
-//    public void setup() throws ServiceException {
-//        MockitoAnnotations.initMocks(this);
-//    }
+    @BeforeClass
+    public void setup() throws ServiceException {
+        MockitoAnnotations.initMocks(this);
+    }
 
     @Test
     public void getFreePlayersTest(){
