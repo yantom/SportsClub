92e437a99cd6488b289d42b684dfba861ca44509
Simon Sudora <simon.sudora@gmail.com>
2016-12-16 16:35:07 +0100

    gitignore fix 2

--- web/target/web/app/components/adminBoard/adminBoardCtrl.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-angular.module("sportsClub").controller('adminBoardCtrl', function($scope,$http) {
-	//console.log($http.get("http://localhost:8080/pa165/rest/auth/10000"));
-});
--- web/target/web/app/components/home/homeCtrl.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-angular.module("sportsClub").controller('homeCtrl', function($scope) {
-
-});
