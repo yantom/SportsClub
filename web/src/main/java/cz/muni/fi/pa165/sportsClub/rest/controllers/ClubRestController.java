65ac88a181527d66b44be60648679d2bbed646ec
Korkoran <david.koncak@seznam.cz>
2016-12-16 13:13:09 +0100

    Club rest controller

---
+++ web/src/main/java/cz/muni/fi/pa165/sportsClub/rest/controllers/ClubRestController.java
@@ -0,0 +1,53 @@
+package cz.muni.fi.pa165.sportsClub.rest.controllers;
+
+import cz.muni.fi.pa165.sportsClub.dto.ClubDto;
+import cz.muni.fi.pa165.sportsClub.dto.PlayerDto;
+import cz.muni.fi.pa165.sportsClub.facade.ClubFacade;
+import java.util.List;
+import javax.inject.Inject;
+import javax.validation.Valid;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ *
+ * @author David Koncak, 410155
+ */
+public class ClubRestController {
+    
+    @Inject
+    ClubFacade clubFacade;
+    
+    @RequestMapping(path = "/create", method = RequestMethod.POST)
+    public ClubDto create(@Valid @RequestBody ClubDto club) {
+        clubFacade.createClub(club);
+        return club;
+    }
+    
+    @RequestMapping(path = "/update", method = RequestMethod.POST)
+    public void update(@Valid @RequestBody ClubDto club) {
+        clubFacade.updateClub(club);
+    }
+
+    @RequestMapping(path = "/{id}", method = RequestMethod.DELETE)
+    public void delete(@PathVariable("id") long id) {
+        clubFacade.deleteClub(id);
+    }
+
+    @RequestMapping(path = "/{id}", method = RequestMethod.GET)
+    public ClubDto findById(@PathVariable("id") long id) {
+        return clubFacade.getClubById(id);
+    }
+
+    @RequestMapping(path = "/findall", method = RequestMethod.GET)
+    public List<ClubDto> findAll() {
+        return clubFacade.getAllClubs();
+    }
+    
+    @RequestMapping(path = "/free", method = RequestMethod.GET)
+    public List<PlayerDto> freePlayers(@PathVariable("id") long id) {
+        return clubFacade.getFreePlayers(id);
+    }
+}
