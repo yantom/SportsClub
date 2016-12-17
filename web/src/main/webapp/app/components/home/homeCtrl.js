88bb7c7d2feb92d60f027ddbca8f0ffcd45eb42b
AndrejBony <andrej.bonis@gmail.com>
2016-12-17 15:42:19 +0100

    Home angular, ctrl, html

--- web/src/main/webapp/app/components/home/homeCtrl.js
+++ web/src/main/webapp/app/components/home/homeCtrl.js
@@ -1,4 +1,21 @@
-"use strict";
-angular.module("sportsClub").controller('homeCtrl', function($scope) {
-
-});
+'use strict';
+ 
+angular.module('sportsClub').controller('homeCtrl',
+    ['$scope', '$rootScope', '$location', 'AuthenticationService',
+    function ($scope, $rootScope, $location, AuthenticationService) {
+        // reset login status
+        AuthenticationService.ClearCredentials();
+ 
+        $scope.login = function () {
+            $scope.dataLoading = true;
+            AuthenticationService.Login($scope.email, $scope.password, function(response) {
+                if(response.success) {
+                    AuthenticationService.SetCredentials($scope.email, $scope.password);
+                    $location.path('/');
+                } else {
+                    $scope.error = response.message;
+                    $scope.dataLoading = false;
+                }
+            });
+        };
+    }]);
